366db63592ee97fa8fec2437e903e434da2a9cfe
F4NT0 <gabriel.stundner@acad.pucrs.br>
2020-12-01 12:24:12 -0300

    added Sonarcloud Script

---
+++ sonarcloud.sh
@@ -0,0 +1,5 @@
+mvn verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar \
+-Dsonar.projectKey=F4NT0_TF_VERIVAL \
+-Dsonar.organization=tfverival \
+-Dsonar.host.url=https://sonarcloud.io \
+-Dsonar.login=6a821f20c048a245f9cc3500d02b77f7734e680a
